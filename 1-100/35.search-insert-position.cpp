f4dbec293bcc6248bc1d16d64dc79ea39990bd3b
chadwick2143 <chenliang2143@qq.com>
2020-03-27 00:46:09 +0800

    add file

---
+++ 1-100/35.search-insert-position.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int lo = 0;
+        int hi = nums.size();
+
+        while (lo < hi)
+        {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] < target)
+            {
+                lo = mid + 1;
+            }
+            else
+            {
+                hi = mid;
+            }
+        }
+
+        return lo;
+    }
+};
